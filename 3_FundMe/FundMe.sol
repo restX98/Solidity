ce1d4f4033afcd98a0c960c06055bdfa2f132034
Enrico Restuccia <enrico.rest98@gmail.com>
2022-07-09 21:19:47 +0200

    3_FundMe: Prelievo

--- 3_FundMe/FundMe.sol
+++ 3_FundMe/FundMe.sol
@@ -22,6 +22,12 @@ contract FundMe {
     address[] public founders;
     mapping(address => uint256) public addressToAmountFounded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         // require controlla se la condizione è verificata e fa procedere il flusso,
         // altrimenti lancia un errore ed esegue un revert (ritorna il gas non utilizzato).
@@ -31,5 +37,30 @@ contract FundMe {
         // msg.sender (address): sender of the message (current call)
         founders.push(msg.sender);
         addressToAmountFounded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        // Resetto la mappa e l'array di sostenitori
+        for(uint i = 0; i<founders.length; i++){
+            addressToAmountFounded[founders[i]] = 0;
+        }
+        founders = new address[](0);
+
+        // Esistono 3 modi per prelevare da uno smart contract:
+        // payable è un tipo particolare di address
+        // address payable myAddress = payable(msg.sender); // payable aggiunge delle funzionalità al tipo address
+        // myAddress.transfer(address(this).balance); // 2300 gas, throws error
+
+        // require(payable(msg.sender).send(address(this).balance), "Call Failed!"); //  2300 gas, returns bool
+
+        // Methodo consigliato
+        (bool callSuccess, /* lowerLevelFunctionReturns */) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed!");
+    }
+
+    modifier onlyOwner(){
+        // _; // Se messo prima lo fa prima, e si può mettere anche più volte?
+        require(msg.sender == owner, "Not Alowed!");
+        _; // Fa il resto del codice
     }
 }
