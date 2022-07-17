79df7fee68775a9deb24f5375671d0eed7913c34
Enrico Restuccia <69401514+restX98@users.noreply.github.com>
2022-07-17 18:22:17 +0200

    Revert "ErrataCorrige"

--- 3_FundMe/FundMe.sol
+++ 3_FundMe/FundMe.sol
@@ -21,7 +21,7 @@ contract FundMe {
 
     uint constant MIN_USD = 50 * 1e18;
 
-    address[] public funders;
+    address[] public founders;
     mapping(address => uint256) public addressToAmountFounded;
 
     address public immutable i_owner; // Rende immutabile il valore dopo la prima assegnazione
@@ -40,16 +40,16 @@ contract FundMe {
         // msg è una variabile speciale
         // msg.value (uint): number of wei sent with the message
         // msg.sender (address): sender of the message (current call)
-        funders.push(msg.sender);
+        founders.push(msg.sender);
         addressToAmountFounded[msg.sender] += msg.value;
     }
 
     function withdraw() public onlyOwner {
         // Resetto la mappa e l'array di sostenitori
-        for (uint i = 0; i < funders.length; i++) {
-            addressToAmountFounded[funders[i]] = 0;
+        for (uint i = 0; i < founders.length; i++) {
+            addressToAmountFounded[founders[i]] = 0;
         }
-        funders = new address[](0);
+        founders = new address[](0);
 
         // Esistono 3 modi per prelevare da uno smart contract:
         // payable è un tipo particolare di address
